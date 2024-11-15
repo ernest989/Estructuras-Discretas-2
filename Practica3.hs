62e47b3866936976545c673ce554c43ff7df66e1
Ernest989 <111658218+ernest989@users.noreply.github.com>
2024-11-14 22:08:40 -0600

    Solo falta el ultimo

--- Practica3.hs
+++ Practica3.hs
@@ -27,42 +27,29 @@ conjunto (Node cabeza (cola)) = if (estaContenido cola cabeza) then (conjunto co
 
 --funcion principal que revisa a condición
 eliminarIndice :: List a -> Int -> List a
-eliminarIndice xs indice = if (indice >= 0 && indice <= ((longitud xs)-1) ) then eliminarIndiceAux xs indice
-	else error "Indice fuera del rango permitido"
+eliminarIndice (Void) _ = error "No hay elementos a eliminar"
+eliminarIndice (Node cabeza (cola)) 0 = (cola)
+eliminarIndice (Node cabeza (cola)) i = if 0<i && i > (longitud (Node cabeza (cola)) -1) 
+	then error "Indice inválido"
+	else (Node cabeza (eliminarIndice cola (i-1)))
 
---funcion auxiliar con índice válido
-eliminarIndiceAux :: List a -> Int -> List a
-eliminarIndiceAux (Void) _ = (Void)
-eliminarIndiceAux (Node cabeza (cola)) 0 = (cola)
-eliminarIndiceAux (Node cabeza (cola)) i = (Node cabeza (eliminarIndice cola (i-1)))
 
---funcion principal que revisa si el indice es correcto
+-- inserta un elemento en el indice especificado de la lista
 insertarIndice :: List a -> Int -> a -> List a
-insertarIndice xs indice e =  if (indice >= 0 && indice <= ((longitud xs)-1) ) then insertarIndiceAux xs indice e
-	else error "Indice fuera del rango permitido"
-
---funcion auxiliar que inserta el elemento
-insertarIndiceAux :: List a -> Int -> a -> List a
-insertarIndiceAux Void _ nuevo = Node nuevo Void  
-insertarIndiceAux (Node cabeza cola) 0 nuevo = Node nuevo (Node cabeza cola)  
-insertarIndiceAux (Node cabeza cola) n nuevo = Node cabeza (insertarIndiceAux cola (n - 1) nuevo) 
-
--- Función para obtener el primer elemento de la lista
-primerElemento :: List a -> a
-primerElemento (Node x _) = x
-
--- fucion para eliminar el primer elemento de la lista
-eliminarPrimero :: List a -> List a
-eliminarPrimero (Node _ cola) = cola
-eliminarPrimero Void = Void  
-
--- funcion para agregar un elemento al final de la lista
-agregarFinal :: a -> List a -> List a
-agregarFinal nuevo Void = Node nuevo Void
-agregarFinal nuevo (Node cabeza cola) = Node cabeza (agregarFinal nuevo cola)
+insertarIndice (Void) i elem = if i==0
+	then (Node elem (Void)) 
+	else error "Índice inváido"
+insertarIndice (Node head tail) 0 elem = Node elem (Node head tail)
+insertarIndice (Node head tail) i elem = if 0<i && i > (longitud (Node head (tail)) -1) 
+	then error "Indice inválido"
+	else Node head (insertarIndice tail (i-1) elem )
 
 -- funcion principal que recorre la lista n veces hacia la derecha
 recorrerLista :: List a -> Int -> List a
-recorrerLista lista 0 = lista  
-recorrerLista Void _ = Void    
-recorrerLista lista n = recorrerLista (agregarFinal (primerElemento lista) (eliminarPrimero lista)) (n - 1)
+recorrerLista lista 0 = lista
+recorrerLista Void _ = Void
+recorrerLista (Node h Void) n = recorrerLista (Node h Void) (n - 1)
+recorrerLista (Node h tail) n = if n<0 
+	then error "parámetro inválido"
+	else insertarIndice (recorrerLista tail (n - 1)) 0 h
+
